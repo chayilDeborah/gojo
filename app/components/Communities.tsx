3b67675c9abe40a5bc9cea4f52d78348ecd08f13
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-08-03 10:19:58 +0100

    fix

--- app/components/Communities.tsx
+++ app/components/Communities.tsx
@@ -18,11 +18,11 @@ const Communities = () => {
                 <h1  className='lg:text-[200px] text-[80px] leading-[90px]  -tracking-[2%] font text-[#D9DAD9] text-center mt-[80px] mb-[58px] '>Community</h1>
                 <div className='lg:flex w-fit mx-auto mb-[50px] lg:mb-[145px]'>
                     <button className='border-[1px] border-[#E6E7E6] flex w-[235px] h-[68px] rounded-[12px]  px-[15.5px] '>
-                        <Image src={twitter} alt='twitter' className='' />
+                        <Image src={twitter} alt='twitter' className='my-auto' />
                         <h1 className='text-[31px] leading-[37.2px] -tracking-[2%] text-white ml-[20px] my-auto  '>X/Twitter</h1>
                     </button>
                     <button className='border-[1px] border-[#E6E7E6] flex w-[235px] h-[68px] rounded-[12px]  px-[15.5px] lg:ml-[60px] lg:mt-[0px] mt-[30px] '>
-                        <Image src={telegram} alt='telegram' className='' />
+                        <Image src={telegram} alt='telegram' className='my-auto' />
                         <h1 className='text-[31px] leading-[37.2px] -tracking-[2%] text-white ml-[20px] my-auto  '>Telegram</h1>
                     </button>
 
