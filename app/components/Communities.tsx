343f5e98bd27303799e2994e7138b9b7533c4789
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-08-03 23:10:17 +0100

    feat: update links

--- app/components/Communities.tsx
+++ app/components/Communities.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import gojo from '../assets/gojo.png'
 import twitter from '../assets/twitter.svg'
 import telegram from '../assets/telegram.svg'
+import whatsapp from '../assets/whatsapp.png'
+import Link from 'next/link'
 
 const Communities = () => {
   return (
@@ -16,15 +18,25 @@ const Communities = () => {
             <div>
                 <Image src={gojo} alt='gojo' className='mt-[50px] lg:-mt-[300px] ' />
                 <h1  className='lg:text-[200px] text-[80px] leading-[90px]  -tracking-[2%] font text-[#D9DAD9] text-center lg:mt-[270px] mt-[80px] lg:mb-[110px] mb-[58px] '>Community</h1>
-                <div className='lg:flex w-fit mx-auto mb-[50px] lg:mb-[145px]'>
+                <div className='lg:flex w-fit mx-auto mb-[80px] lg:mb-[145px]'>
+                    <Link href='https://x.com/thegojobot'>
                     <button className='border-[1px] border-[#E6E7E6] flex w-[235px] h-[68px] rounded-[12px]  px-[15.5px] '>
                         <Image src={twitter} alt='twitter' className='my-auto' />
-                        <h1 className='text-[31px] leading-[37.2px] -tracking-[2%] text-white ml-[20px] my-auto  '>X/Twitter</h1>
+                        <h1 className='text-[31px] leading-[37.2px] -tracking-[2%] text-white ml-[20px] my-auto   hover:text-[#FF7878]'>X/Twitter</h1>
                     </button>
+                    </Link>
+                    <Link href='https://t.me/gojobotportal'>
                     <button className='border-[1px] border-[#E6E7E6] flex w-[235px] h-[68px] rounded-[12px]  px-[15.5px] lg:ml-[60px] lg:mt-[0px] mt-[30px] '>
                         <Image src={telegram} alt='telegram' className='my-auto' />
-                        <h1 className='text-[31px] leading-[37.2px] -tracking-[2%] text-white ml-[20px] my-auto  '>Telegram</h1>
+                        <h1 className='text-[31px] leading-[37.2px] -tracking-[2%] text-white ml-[20px] my-auto  hover:text-[#FF7878] '>Telegram</h1>
                     </button>
+                    </Link>
+                    <Link href='https://bit.ly/gojobot'>
+                    <button className='border-[1px] border-[#E6E7E6] flex w-[235px] h-[68px] rounded-[12px]  px-[15.5px] lg:ml-[60px] lg:mt-[0px] mt-[30px] '>
+                        <Image src={whatsapp} alt='whatsapp' className='my-auto w-[48px] ' />
+                        <h1 className='text-[31px] leading-[37.2px] -tracking-[2%] text-white ml-[10px] my-auto  hover:text-[#FF7878]  '>WhatsApp</h1>
+                    </button>
+                    </Link>
 
 
                 </div>
